a8fd6727276201819f74a177315dd2affa7e5183
Wizzard87 <wizardgamessup@outlook.com>
2025-10-13 18:11:48 +0300

    Create old_data.php

---
+++ old_data.php
@@ -0,0 +1,33 @@
+{
+  "servers": [
+    {
+      "serverID": "0",
+      "name": "МОСКВА",
+      "online": "376",
+      "mult": "x2",
+      "region": "Europe",
+      "color": "#fdd835",
+      "maxonline": "1000",
+      "ip": "62.122.213.60",
+      "port": "7777",
+      "onlinePer30Min": "1229",
+      "onlinePer1Day": "4487",
+      "onlinePer7Day": "0",
+      "onlinePerMouth": "10650",
+      "locked": "0",
+      "develop": "0",
+      "open_at": "0",
+      "only_langs": [
+        "ru", "uk", "be", "kk", "uz", "ky", "tg", "tk", "az", "hy", "ro"
+      ],
+      "except_langs": null
+    }
+  ],
+  "news": [
+    {
+      "imageurl": "https://files.liverussia.online/historyforlauncher/history75.png",
+      "title": "Работа Дайвера",
+      "url": "https://t.me/lr_dev/744"
+    }
+  ]
+}
