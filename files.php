befefd3285674080f9ee5cd1d7577fbcc4142a10
Wizzard87 <wizardgamessup@outlook.com>
2025-10-14 18:50:15 +0300

    Update files.php

--- files.php
+++ files.php
@@ -1,34 +1,9 @@
 {
   "files": [
     {
-      "path": "anim-auido",
-      "size": 60873070,
-      "hash": 629713277
-    },
-    {
-      "path": "data-models",
-      "size": 4522845,
-      "hash": 208302139
-    },
-    {
-      "path": "files",
-      "size": 94061,
-      "hash": 3071652697
-    },
-    {
-      "path": "SAMP",
-      "size": 4248336,
-      "hash": 1544392279
-    },
-    {
-      "path": "texdb",
-      "size": 1114379380,
-      "hash": 3563580555
-    },
-    {
-      "path": "text-textures",
-      "size": 2924409,
-      "hash": 3880359977
+      "path": "root.zip",
+      "size": 1119475271,
+      "hash": 3784803437
     }
   ]
 }
