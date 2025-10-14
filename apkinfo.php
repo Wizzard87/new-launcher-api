6ecd81e8dc718a268ebdb841d106dc7d134387e5
Wizzard87 <wizardgamessup@outlook.com>
2025-10-14 11:39:11 +0300

    Update apkinfo.php

--- apkinfo.php
+++ apkinfo.php
@@ -1,5 +1,5 @@
 {
-  "version": "1258",
+  "version": "1",
   "link": "https://files.liverussia.online/apk/test/app-ver_test-release.apk",
   "path": "files/temp_downloads/app-ver_test-release.apk",
   "size": 74222324,
